9e268d0492f841fb3e095ccdb458d915a467759b
Stéphane <s71truchot@gmail.com>
2020-12-09 11:45:22 +0100

    Fin exercice 1

---
+++ Exercice 1/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1> Exercice 1 </h1>
+
+    <?php
+
+    echo       '<ul>
+                    <li>Banane</li>
+                    <li>Fraise</li>
+                    <li>Kiwi</li>
+                    <li>Papaye</li>
+                    <li>Coco</li>
+                </ul>';
+
+    ?>
+
+
+</body>
+</html>
