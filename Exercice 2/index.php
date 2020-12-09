14c4bfcdd73fc312db3e3e0b84526cff5c0277e1
Stéphane <s71truchot@gmail.com>
2020-12-09 13:42:28 +0100

    fin exercice 2

---
+++ Exercice 2/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+    $name = 'Stéphane';
+    $color = 'orange';
+
+    echo '<h1> Exercice 2 Salutations '.$name.' </h1>';
+    echo '<body style="background-color:'.$color.'">';
+
+
+
+    ?>
+
+
+</body>
+</html>
