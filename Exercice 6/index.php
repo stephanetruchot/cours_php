2d68f0f7d47a33fa5be18b32e36720e03935ca6c
Stéphane <s71truchot@gmail.com>
2020-12-10 10:33:31 +0100

    fin exercice6

---
+++ Exercice 6/index.php
@@ -0,0 +1,26 @@
+<?php
+    $names = ['Pierre', 'Paul', 'Jacques', 'Kiki', 'Philippe', 'Morgane', 'Marie', 'Anne', 'Murielle', 'Gertrude'];
+    $i = 0;
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+
+        <?php
+
+            for($i = 0; $i < 10 ; $i++){
+                echo '<li>'.$names[$i].'</li>';
+            }
+
+
+        ?>
+
+    </ul>
+</body>
+</html>
